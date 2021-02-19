45238fc6c89f8fd2f5b7303f62ac3d08a97d4faa
Sune Carelse <sune.carelse@takealot.com>
2021-02-19 20:10:29 +0200

    header leaft/right with Avatar timme seacrh icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Header.css";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
@@ -10,15 +12,45 @@ function Header() {
         <HeaderAvatar
         //  Add onClick
         />
+        <AccessTimeIcon />
       </HeaderLeft>
+      <HeaderSearchIcon>
+        <SearchIcon />
+        <input placeholder="Search PAPAFAM" />
+      </HeaderSearchIcon>
     </HeaderContainer>
   );
 }
 
 export default Header;
 
-const HeaderContainer = styled.div``;
+const HeaderSearchIcon = styled.div``;
 
-const HeaderLeft = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
 
-const HeaderAvatar = styled(Avatar)``;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
