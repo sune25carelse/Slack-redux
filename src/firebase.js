3b0bb8fff22e9d2009a715183e023d9852552945
Sune Carelse <sune.carelse@takealot.com>
2021-02-19 21:33:39 +0200

    setup firebase auth db and provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCtl3vguJA3FO1--rJ3qnP9NwXQSgEvY84",
   authDomain: "slack-redux-180d3.firebaseapp.com",
@@ -6,3 +8,12 @@ const firebaseConfig = {
   messagingSenderId: "327546266421",
   appId: "1:327546266421:web:a3b7e67c2579150cee627c",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
