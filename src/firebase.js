3ea1ff535bdfe8591c0d523cbf612990270cd243
Sune Carelse <sune.carelse@takealot.com>
2021-02-19 19:39:03 +0200

    firebase tools installed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtl3vguJA3FO1--rJ3qnP9NwXQSgEvY84",
+  authDomain: "slack-redux-180d3.firebaseapp.com",
+  projectId: "slack-redux-180d3",
+  storageBucket: "slack-redux-180d3.appspot.com",
+  messagingSenderId: "327546266421",
+  appId: "1:327546266421:web:a3b7e67c2579150cee627c",
+};
