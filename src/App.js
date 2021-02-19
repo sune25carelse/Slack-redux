23d66864a13c6d13511ac8ea41b69fe4dbb9ffe0
Sune Carelse <sune.carelse@takealot.com>
2021-02-19 19:18:51 +0200

    react router dom and exact path defined

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets build Slack-redux</h1>
+    <div className="app">
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
