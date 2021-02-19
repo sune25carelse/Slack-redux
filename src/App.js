6db758b1731f78cc42e980f3267537fa13674515
Sune Carelse <sune.carelse@takealot.com>
2021-02-19 21:15:52 +0200

    created Appbody sidebar  and style appBdy

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact></Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -20,3 +24,8 @@ function App() {
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
