c5484aae20ef1fde1073f987d0c9a518e2cb459f
Sune Carelse <sune.carelse@takealot.com>
2021-02-20 12:32:07 +0200

    styled header sections

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -10,8 +10,8 @@ function Chat() {
         <HeaderLeft>
           <h4>
             <strong>#Room-name</strong>
-            <StarBorderOutlinedIcon />
           </h4>
+          <StarBorderOutlinedIcon />
         </HeaderLeft>
         <HeaderRight>
           <p>
@@ -25,11 +25,41 @@ function Chat() {
 
 export default Chat;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
 
-const HeaderRight = styled.div``;
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+  }
+
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 10px;
+    font-size: 18px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+
+  > p > .MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size: 16px;
+  }
+`;
 
 const ChatContainer = styled.div`
   flex: 0.7;
