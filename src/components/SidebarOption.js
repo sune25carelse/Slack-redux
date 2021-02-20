39c14bd41d764723df127a0699b54eb9f0015f78
Sune Carelse <sune.carelse@takealot.com>
2021-02-20 12:22:16 +0200

    sidebarOptn add channel in db function

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,9 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import db from "../firebase";
+import { enterRoom } from "../features/appSlice";
 
-function SidebarOption({ Icon, title, addChannel }) {
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
+
   return (
-    <SidebarOptionContainer>
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
       {Icon ? (
         <h3>{title}</h3>
@@ -38,4 +64,7 @@ const SidebarOptionContainer = styled.div`
   }
 `;
 
-const SidebarOptionChannel = styled.div``;
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
+`;
