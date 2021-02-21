2dc693af9336ce829077d98ea3aacbb4d7ebbb39
Sune Carelse <sune.carelse@takealot.com>
2021-02-21 20:22:00 +0200

    add to db, useState, setInput functionality

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,17 +1,41 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import styled from "styled-components";
+import db from "../firebase";
+import firebase from "firebase";
+import { useState } from "react";
 
 function ChatInput({ channelName, channelId }) {
+  const [input, setInput] = useState("");
+
   const sendMessage = (e) => {
     e.preventDefault(); // prevent refresh
+
+    if (!channelId) {
+      return false;
+    }
+
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: "Luca Van Wyk",
+      userImage:
+        "https://seeklogo.net/wp-content/uploads/2011/08/liverpool-logo-vector-400x400.png",
+    });
+
+    setInput("");
   };
+
   return (
     <ChatInputContainer>
       <form>
-        <input placeholder={`Message #ROOM`} />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={`Message #${channelName}`}
+        />
         <Button hidden type="submit" onClick={sendMessage}>
-          SEND MESSAGE
+          SEND
         </Button>
       </form>
     </ChatInputContainer>
@@ -20,4 +44,26 @@ function ChatInput({ channelName, channelId }) {
 
 export default ChatInput;
 
-const ChatInputContainer = styled.div``;
+const ChatInputContainer = styled.div`
+  border-radius: 20px;
+
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    bottom: 30px;
+    width: 60%;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > button {
+    display: none !important;
+  }
+`;
