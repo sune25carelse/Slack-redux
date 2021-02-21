0665d24c4c4153b72777344ffb75dfc904403a42
Sune Carelse <sune.carelse@takealot.com>
2021-02-21 21:16:17 +0200

    login page with image and rendered in app

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <LoginContainer>
+      <LoginInnerContainer>
+        <img
+          src="https://yt3.ggpht.com/a-/AAuE7mDBBS00E88tbJONrBTNiWDBhHSfBKSM3889QA=s900-mo-c-c0xffffffff-rj-k-no"
+          alt=""
+        />
+      </LoginInnerContainer>
+    </LoginContainer>
+  );
+}
+
+export default Login;
+
+const LoginContainer = styled.div``;
+
+const LoginInnerContainer = styled.div``;
