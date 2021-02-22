f8d1dfb2d895939c95707f00647ebef9cbad206c
Sune Carelse <sune.carelse@takealot.com>
2021-02-22 21:26:00 +0200

    login created with button and auth google

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
 
 function Login() {
+  const signIn = (e) => {
+    e.preventDefault();
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
+
   return (
     <LoginContainer>
       <LoginInnerContainer>
@@ -9,6 +16,10 @@ function Login() {
           src="https://yt3.ggpht.com/a-/AAuE7mDBBS00E88tbJONrBTNiWDBhHSfBKSM3889QA=s900-mo-c-c0xffffffff-rj-k-no"
           alt=""
         />
+        <h1>Sign in to the PAPA FAM</h1>
+        <p>luca.slack.com</p>
+
+        <Button onClick={signIn}>Sign In to the PAPAFAM</Button>
       </LoginInnerContainer>
     </LoginContainer>
   );
@@ -16,6 +27,30 @@ function Login() {
 
 export default Login;
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+  background-color: #f8f8f8;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
 
-const LoginInnerContainer = styled.div``;
+const LoginInnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+  > img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+
+  > button {
+    margin-top: 50px;
+    text-transform: inherit !important;
+    background-color: #0a8d48 !important;
+    color: white;
+  }
+`;
